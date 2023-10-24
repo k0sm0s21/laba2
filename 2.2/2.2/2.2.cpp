542be979d6cb96fb64f7a2431a993dcc94a7540d
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-24 10:39:02 +0300

    Add files via upload

---
+++ 2.2/2.2/2.2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int dayNumber, z = 0;
+	do {
+		cout << "Введите номер недели: ";
+		cin >> dayNumber;
+		cout << endl;
+		if (dayNumber > 7) {
+			cout << "Такого дня недели не существует\nВведите номер повторно: ";
+			cin >> dayNumber;
+		}
+		if (dayNumber == 1) {
+			cout << "Понедельник: \n Практика \n";
+		}
+		if (dayNumber == 2) {
+			cout << "Вторник: \n Практика \n";
+		}
+		if (dayNumber == 3) {
+			cout << "Среда: \n Практика \n";
+		}
+		if (dayNumber == 4) {
+			cout << "Четверг: \n Практика \n";
+		}
+		if (dayNumber == 5) {
+			cout << "Пятница: \n Практика \n";
+		}
+		if (dayNumber == 6) {
+			cout << "Суббота: \n Работа \n";
+		}
+		if (dayNumber == 7) {
+			cout << "Воскресенье: \n Отдых \n";
+		}
+	} while (z < 7);
+
+	return 0;
+}
