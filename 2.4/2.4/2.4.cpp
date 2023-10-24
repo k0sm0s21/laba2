95241c8aec99fd7f1640d0f318140ed75d4acdc5
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-24 11:23:51 +0300

    Add files via upload

---
+++ 2.4/2.4/2.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    char ch;
+    short int a, b, itog;
+    cout << "Введите число:" << endl;
+    cin >> a;
+    cout << "Введите второе число:" << endl;
+    cin >> b;
+    cout << "Результат:" << endl;
+    cin >> itog;
+    if (itog == a * b) {
+        cout << "Правильно";
+    }
+    else cout << "Неверно!!!Правильный ответ: " << a * b;
+    return 0;
+
+}
