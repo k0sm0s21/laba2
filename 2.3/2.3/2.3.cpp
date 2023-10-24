fcb5cc18ae28a2935646a1205b5d67485b42f434
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-24 11:22:35 +0300

    Add files via upload

---
+++ 2.3/2.3/2.3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+    setlocale(0, "");
+    char station;
+
+    cout << "Введите букву станции: ";
+    cin >> station;
+
+    station = tolower(station);
+
+    switch (station) {
+    case 'a':
+    case 'b':
+    case 'c':
+    case 'd':
+    case 'e': {
+        cout << "Время в пути до станции " << station << " составляет 10 минут." << endl;
+        break;
+    }
+    default: {
+        cout << "Неизвестная станция." << endl;
+        break;
+    }
+    }
+
+    return 0;
+}
