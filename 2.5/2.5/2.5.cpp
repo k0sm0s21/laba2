da67c33edda3ccea6a56288758b7bcc66f59f32f
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-24 12:07:56 +0300

    Add files via upload

---
+++ 2.5/2.5/2.5.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int finger;
+    cout << "Введите порядковый номер пальца: ";
+    cin >> finger;
+    switch (finger) {
+    case 1: {
+        cout << "Большой палец";
+        break;
+    }
+    case 2: {
+        cout << "Указательный палец";
+        break;
+    }
+    case 3: {
+        cout << "Средний палец";
+        break;
+    }
+    case 4: {
+        cout << "Безымянный палец";
+        break;
+    }
+    case 5: {
+        cout << "Мизинец";
+        break;
+    }
+    default: {
+        cout << "Неверный номер пальца";
+        break;
+    }
+    }
+
+    return 0;
+}
